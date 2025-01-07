3eb77b4dcc96332d60c27352c6fdafcfefd69334
gitfoundation1 <gitfoundation1@gmail.com>
2025-01-07 09:58:12 +0530

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,9 @@
+public class Calc {
+    public static void main(String[] args) {
+
+        Addition.add();
+        Substraction.sub();
+        Multiply.multiply();
+        Division.div();
+    }
+}
